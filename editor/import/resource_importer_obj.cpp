4b64fb08becfa6f6565185a37ff64702500df1df
MrMartinIden <MrMartinIden@protonmail.com>
2020-03-22 10:33:39 +0300

    editor/import/resource_importer_obj.cpp

--- editor/import/resource_importer_obj.cpp
+++ editor/import/resource_importer_obj.cpp
@@ -68,7 +68,7 @@ static Error _parse_material_library(const String &p_path, Map<String, Ref<Spati
 		} else if (l.begins_with("Kd ")) {
 			//normal
 			ERR_FAIL_COND_V(current.is_null(), ERR_FILE_CORRUPT);
-			Vector<String> v = l.split(" ", false);
+			std::vector<String> v = l.split(" ", false);
 			ERR_FAIL_COND_V(v.size() < 4, ERR_INVALID_DATA);
 			Color c = current->get_albedo();
 			c.r = v[1].to_float();
@@ -78,7 +78,7 @@ static Error _parse_material_library(const String &p_path, Map<String, Ref<Spati
 		} else if (l.begins_with("Ks ")) {
 			//normal
 			ERR_FAIL_COND_V(current.is_null(), ERR_FILE_CORRUPT);
-			Vector<String> v = l.split(" ", false);
+			std::vector<String> v = l.split(" ", false);
 			ERR_FAIL_COND_V(v.size() < 4, ERR_INVALID_DATA);
 			float r = v[1].to_float();
 			float g = v[2].to_float();
@@ -88,14 +88,14 @@ static Error _parse_material_library(const String &p_path, Map<String, Ref<Spati
 		} else if (l.begins_with("Ns ")) {
 			//normal
 			ERR_FAIL_COND_V(current.is_null(), ERR_FILE_CORRUPT);
-			Vector<String> v = l.split(" ", false);
+			std::vector<String> v = l.split(" ", false);
 			ERR_FAIL_COND_V(v.size() != 2, ERR_INVALID_DATA);
 			float s = v[1].to_float();
 			current->set_metallic((1000.0 - s) / 1000.0);
 		} else if (l.begins_with("d ")) {
 			//normal
 			ERR_FAIL_COND_V(current.is_null(), ERR_FILE_CORRUPT);
-			Vector<String> v = l.split(" ", false);
+			std::vector<String> v = l.split(" ", false);
 			ERR_FAIL_COND_V(v.size() != 2, ERR_INVALID_DATA);
 			float d = v[1].to_float();
 			Color c = current->get_albedo();
@@ -107,7 +107,7 @@ static Error _parse_material_library(const String &p_path, Map<String, Ref<Spati
 		} else if (l.begins_with("Tr ")) {
 			//normal
 			ERR_FAIL_COND_V(current.is_null(), ERR_FILE_CORRUPT);
-			Vector<String> v = l.split(" ", false);
+			std::vector<String> v = l.split(" ", false);
 			ERR_FAIL_COND_V(v.size() != 2, ERR_INVALID_DATA);
 			float d = v[1].to_float();
 			Color c = current->get_albedo();
@@ -215,9 +215,9 @@ static Error _parse_obj(const String &p_path, List<Ref<Mesh> > &r_meshes, bool p
 	Vector3 scale_mesh = p_scale_mesh;
 	int mesh_flags = p_optimize ? Mesh::ARRAY_COMPRESS_DEFAULT : 0;
 
-	Vector<Vector3> vertices;
-	Vector<Vector3> normals;
-	Vector<Vector2> uvs;
+	std::vector<Vector3> vertices;
+	std::vector<Vector3> normals;
+	std::vector<Vector2> uvs;
 	String name;
 
 	Map<String, Map<String, Ref<SpatialMaterial> > > material_map;
@@ -242,7 +242,7 @@ static Error _parse_obj(const String &p_path, List<Ref<Mesh> > &r_meshes, bool p
 
 		if (l.begins_with("v ")) {
 			//vertex
-			Vector<String> v = l.split(" ", false);
+			std::vector<String> v = l.split(" ", false);
 			ERR_FAIL_COND_V(v.size() < 4, ERR_FILE_CORRUPT);
 			Vector3 vtx;
 			vtx.x = v[1].to_float() * scale_mesh.x;
@@ -251,7 +251,7 @@ static Error _parse_obj(const String &p_path, List<Ref<Mesh> > &r_meshes, bool p
 			vertices.push_back(vtx);
 		} else if (l.begins_with("vt ")) {
 			//uv
-			Vector<String> v = l.split(" ", false);
+			std::vector<String> v = l.split(" ", false);
 			ERR_FAIL_COND_V(v.size() < 3, ERR_FILE_CORRUPT);
 			Vector2 uv;
 			uv.x = v[1].to_float();
@@ -260,7 +260,7 @@ static Error _parse_obj(const String &p_path, List<Ref<Mesh> > &r_meshes, bool p
 
 		} else if (l.begins_with("vn ")) {
 			//normal
-			Vector<String> v = l.split(" ", false);
+			std::vector<String> v = l.split(" ", false);
 			ERR_FAIL_COND_V(v.size() < 4, ERR_FILE_CORRUPT);
 			Vector3 nrm;
 			nrm.x = v[1].to_float();
@@ -270,18 +270,18 @@ static Error _parse_obj(const String &p_path, List<Ref<Mesh> > &r_meshes, bool p
 		} else if (l.begins_with("f ")) {
 			//vertex
 
-			Vector<String> v = l.split(" ", false);
+			std::vector<String> v = l.split(" ", false);
 			ERR_FAIL_COND_V(v.size() < 4, ERR_FILE_CORRUPT);
 
 			//not very fast, could be sped up
 
-			Vector<String> face[3];
+			std::vector<String> face[3];
 			face[0] = v[1].split("/");
 			face[1] = v[2].split("/");
 			ERR_FAIL_COND_V(face[0].size() == 0, ERR_FILE_CORRUPT);
 
 			ERR_FAIL_COND_V(face[0].size() != face[1].size(), ERR_FILE_CORRUPT);
-			for (int i = 2; i < v.size() - 1; i++) {
+			for (decltype(v.size()) i = 2; i < v.size() - 1; ++i) {
 
 				face[2] = v[i + 1].split("/");
 
