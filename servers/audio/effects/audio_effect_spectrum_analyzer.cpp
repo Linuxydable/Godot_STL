579f744be18fb17b694a2a6b7f824dc2fabf2ff4
MrMartinIden <MrMartinIden@protonmail.com>
2020-03-25 22:48:33 +0300

    servers/audio/effects/audio_effect_spectrum_analyzer

--- servers/audio/effects/audio_effect_spectrum_analyzer.cpp
+++ servers/audio/effects/audio_effect_spectrum_analyzer.cpp
@@ -109,9 +109,9 @@ void AudioEffectSpectrumAnalyzerInstance::process(const AudioFrame *p_src_frames
 		int to_fill = fft_size * 2 - temporal_fft_pos;
 		to_fill = MIN(to_fill, p_frame_count);
 
-		float *fftw = temporal_fft.ptrw();
+		float *fftw = temporal_fft.data();
 		for (int i = 0; i < to_fill; i++) { //left and right buffers
-			float window = -0.5 * Math::cos(2.0 * Math_PI * (double)i / (double)to_fill) + 0.5;
+			float window = -0.5 * Math::cos(Math_TAU * (double)i / (double)to_fill) + 0.5;
 			fftw[(i + temporal_fft_pos) * 2] = window * p_src_frames[i].l;
 			fftw[(i + temporal_fft_pos) * 2 + 1] = 0;
 			fftw[(i + temporal_fft_pos + fft_size * 2) * 2] = window * p_src_frames[i].r;
@@ -128,7 +128,7 @@ void AudioEffectSpectrumAnalyzerInstance::process(const AudioFrame *p_src_frames
 			smbFft(fftw + fft_size * 4, fft_size * 2, -1);
 			int next = (fft_pos + 1) % fft_count;
 
-			AudioFrame *hw = (AudioFrame *)fft_history[next].ptr(); //do not use write, avoid cow
+			AudioFrame *hw = (AudioFrame *)fft_history[next].data(); //do not use write, avoid cow
 
 			for (int i = 0; i < fft_size; i++) {
 				//abs(vec)/fft_size normalizes each frequency
@@ -183,7 +183,7 @@ Vector2 AudioEffectSpectrumAnalyzerInstance::get_magnitude_for_frequency_range(f
 	if (begin_pos > end_pos) {
 		SWAP(begin_pos, end_pos);
 	}
-	const AudioFrame *r = fft_history[fft_index].ptr();
+	const AudioFrame *r = fft_history[fft_index].data();
 
 	if (p_mode == MAGNITUDE_AVERAGE) {
 		Vector2 avg;
@@ -222,10 +222,10 @@ Ref<AudioEffectInstance> AudioEffectSpectrumAnalyzer::instance() {
 	ins->fft_history.resize(ins->fft_count);
 	ins->temporal_fft.resize(ins->fft_size * 8); //x2 stereo, x2 amount of samples for freqs, x2 for input
 	ins->temporal_fft_pos = 0;
-	for (int i = 0; i < ins->fft_count; i++) {
-		ins->fft_history.write[i].resize(ins->fft_size); //only magnitude matters
-		for (int j = 0; j < ins->fft_size; j++) {
-			ins->fft_history.write[i].write[j] = AudioFrame(0, 0);
+	for (auto &&frames : ins->fft_history) {
+		frames.resize(ins->fft_size); //only magnitude matters
+		for (auto &&frame : frames) {
+			frame = AudioFrame(0, 0);
 		}
 	}
 	return ins;
