11ff87c5c93d756007dd15aa511610d7dbbf6f97
theludovyc <theludovyc@gmail.com>
2020-03-26 13:29:02 +0100

    core\io\image_loader.cpp

--- core/io/image_loader.cpp
+++ core/io/image_loader.cpp
@@ -30,6 +30,8 @@
 
 #include "image_loader.h"
 
+#include <algorithm>
+
 #include "core/print_string.h"
 
 bool ImageFormatLoader::recognize(const String &p_extension) const {
@@ -111,8 +113,13 @@ void ImageLoader::add_image_format_loader(ImageFormatLoader *p_loader) {
 }
 
 void ImageLoader::remove_image_format_loader(ImageFormatLoader *p_loader) {
+	auto it_find = std::find(loader.begin(), loader.end(), p_loader);
 
-	loader.erase(p_loader);
+	if (it_find != loader.end()) {
+		loader.erase(it_find);
+	}
+
+	// need_update : return warn when not find
 }
 
 const std::vector<ImageFormatLoader *> &ImageLoader::get_image_format_loaders() {
