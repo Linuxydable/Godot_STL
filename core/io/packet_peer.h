ad364fd28592aba528adc0106fb2bd133b3b70fa
theludovyc <theludovyc@gmail.com>
2020-04-07 23:24:14 +0200

    core\io\packet_peer.h

--- core/io/packet_peer.h
+++ core/io/packet_peer.h
@@ -45,14 +45,14 @@ class PacketPeer : public Reference {
 
 	static void _bind_methods();
 
-	Error _put_packet(const Vector<uint8_t> &p_buffer);
-	Vector<uint8_t> _get_packet();
+	Error _put_packet(const std::vector<uint8_t> &p_buffer);
+	std::vector<uint8_t> _get_packet();
 	Error _get_packet_error() const;
 
 	mutable Error last_get_error;
 
 	int encode_buffer_max_size;
-	Vector<uint8_t> encode_buffer;
+	std::vector<uint8_t> encode_buffer;
 
 public:
 	virtual int get_available_packet_count() const = 0;
@@ -63,8 +63,8 @@ public:
 
 	/* helpers / binders */
 
-	virtual Error get_packet_buffer(Vector<uint8_t> &r_buffer);
-	virtual Error put_packet_buffer(const Vector<uint8_t> &p_buffer);
+	virtual Error get_packet_buffer(std::vector<uint8_t> &r_buffer);
+	virtual Error put_packet_buffer(const std::vector<uint8_t> &p_buffer);
 
 	virtual Error get_var(Variant &r_variant, bool p_allow_objects = false);
 	virtual Error put_var(const Variant &p_packet, bool p_full_objects = false);
