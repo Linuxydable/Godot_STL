1d9bca8235bddfc648539dae07d6691325285595
theludovyc <theludovyc@gmail.com>
2020-04-18 01:46:10 +0200

    editor\editor_folding

--- editor/editor_folding.h
+++ editor/editor_folding.h
@@ -35,8 +35,8 @@
 
 class EditorFolding {
 
-	Vector<String> _get_unfolds(const Object *p_object);
-	void _set_unfolds(Object *p_object, const Vector<String> &p_unfolds);
+	std::vector<String> _get_unfolds(const Object *p_object);
+	void _set_unfolds(Object *p_object, const std::vector<String> &p_unfolds);
 
 	void _fill_folds(const Node *p_root, const Node *p_node, Array &p_folds, Array &resource_folds, Array &nodes_folded, Set<RES> &resources);
 
