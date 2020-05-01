0a61aabbf3bd47c03f94367e7f2f10c7643f46e7
theludovyc <theludovyc@gmail.com>
2020-05-01 20:03:33 +0200

    core\math\triangulate

--- core/math/triangulate.cpp
+++ core/math/triangulate.cpp
@@ -30,7 +30,7 @@
 
 #include "triangulate.h"
 
-real_t Triangulate::get_area(const Vector<Vector2> &contour) {
+real_t Triangulate::get_area(const std::vector<Vector2> &contour) {
 
 	int n = contour.size();
 	const Vector2 *c = &contour[0];
@@ -82,7 +82,7 @@ bool Triangulate::is_inside_triangle(real_t Ax, real_t Ay,
 	}
 };
 
-bool Triangulate::snip(const Vector<Vector2> &p_contour, int u, int v, int w, int n, const Vector<int> &V, bool relaxed) {
+bool Triangulate::snip(const std::vector<Vector2> &p_contour, int u, int v, int w, int n, const std::vector<int> &V, bool relaxed) {
 	int p;
 	real_t Ax, Ay, Bx, By, Cx, Cy, Px, Py;
 	const Vector2 *contour = &p_contour[0];
@@ -115,23 +115,23 @@ bool Triangulate::snip(const Vector<Vector2> &p_contour, int u, int v, int w, in
 	return true;
 }
 
-bool Triangulate::triangulate(const Vector<Vector2> &contour, Vector<int> &result) {
+bool Triangulate::triangulate(const std::vector<Vector2> &contour, std::vector<int> &result) {
 	/* allocate and initialize list of Vertices in polygon */
 
 	int n = contour.size();
 	if (n < 3) return false;
 
-	Vector<int> V;
+	std::vector<int> V;
 	V.resize(n);
 
 	/* we want a counter-clockwise polygon in V */
 
 	if (0.0 < get_area(contour))
 		for (int v = 0; v < n; v++)
-			V.write[v] = v;
+			V[v] = v;
 	else
 		for (int v = 0; v < n; v++)
-			V.write[v] = (n - 1) - v;
+			V[v] = (n - 1) - v;
 
 	bool relaxed = false;
 
@@ -182,7 +182,7 @@ bool Triangulate::triangulate(const Vector<Vector2> &contour, Vector<int> &resul
 
 			/* remove v from remaining polygon */
 			for (s = v, t = v + 1; t < nv; s++, t++)
-				V.write[s] = V[t];
+				V[s] = V[t];
 
 			nv--;
 
