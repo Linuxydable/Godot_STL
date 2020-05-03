a5325c8a4e028e25435d6784fd7cc3a9a848b502
theludovyc <theludovyc@gmail.com>
2020-05-03 11:34:06 +0200

    helper\std_h.h

--- helper/std_h.h
+++ helper/std_h.h
@@ -23,4 +23,15 @@ namespace std_h {
 		}
 		return false;
 	}
+
+	// need_update : bad code to help recode, need to be erased in futur
+	template <typename T>
+	static int getIndex(const std::vector<T>& v, const T& value) {
+		auto it_find = std::find(v.begin(), v.end(), value);
+
+		if (it_find != v.end()) {
+			return std::distance(v.begin(), it_find);
+		}
+		return -1;
+	}
 }
