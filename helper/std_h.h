83c8a4707be0faed1bccebeb8af54a2a91ac6c8c
MrMartinIden <MrMartinIden@protonmail.com>
2020-05-03 21:20:06 +0300

    helper/std_h.h

--- helper/std_h.h
+++ helper/std_h.h
@@ -39,7 +39,7 @@ namespace std_h {
 	}
 
 	template <typename T>
-	static void appendArray(std::vector<T> &v1, std::vector<T> &v2) {
+	static void appendArray(std::vector<T> &v1, const std::vector<T> &v2) {
 		v1.insert(v1.end(), v2.begin(), v2.end());
 	}
 }
