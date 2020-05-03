c43a6e51e89319c7120ff0a69eb789ad76857b98
theludovyc <theludovyc@gmail.com>
2020-05-03 19:24:57 +0200

    core\io\marshalls.cpp

--- core/io/marshalls.cpp
+++ core/io/marshalls.cpp
@@ -337,8 +337,8 @@ Error decode_variant(Variant &r_variant, const uint8_t *p_buffer, int p_len, int
 			if (strlen & 0x80000000) {
 				//new format
 				ERR_FAIL_COND_V(len < 12, ERR_INVALID_DATA);
-				Vector<StringName> names;
-				Vector<StringName> subnames;
+				std::vector<StringName> names;
+				std::vector<StringName> subnames;
 
 				uint32_t namecount = strlen &= 0x7FFFFFFF;
 				uint32_t subnamecount = decode_uint32(buf + 4);
