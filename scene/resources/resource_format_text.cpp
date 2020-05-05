0815684f73f7156a60664c2f8fe86ab3242a4244
theludovyc <theludovyc@gmail.com>
2020-05-05 16:45:38 +0200

    scene\resources\resource_format_text.cpp

--- scene/resources/resource_format_text.cpp
+++ scene/resources/resource_format_text.cpp
@@ -332,7 +332,7 @@ Ref<PackedScene> ResourceInteractiveLoaderText::_parse_node_tag(VariantParser::R
 				binds = next_tag.fields["binds"];
 			}
 
-			Vector<int> bind_ints;
+			std::vector<int> bind_ints;
 			for (int i = 0; i < binds.size(); i++) {
 				bind_ints.push_back(packed_scene->get_state()->add_value(binds[i]));
 			}
@@ -1008,8 +1008,8 @@ Error ResourceInteractiveLoaderText::save_as_binary(FileAccess *p_f, const Strin
 		return ERR_CANT_OPEN;
 	}
 
-	Vector<size_t> local_offsets;
-	Vector<size_t> local_pointers_pos;
+	std::vector<size_t> local_offsets;
+	std::vector<size_t> local_pointers_pos;
 
 	while (next_tag.name == "sub_resource" || next_tag.name == "resource") {
 
@@ -1158,8 +1158,8 @@ Error ResourceInteractiveLoaderText::save_as_binary(FileAccess *p_f, const Strin
 
 	wf->seek_end();
 
-	Vector<uint8_t> data = FileAccess::get_file_as_array(temp_file);
-	wf->store_buffer(data.ptr(), data.size());
+	std::vector<uint8_t> data = FileAccess::get_file_as_array(temp_file);
+	wf->store_buffer(data.data(), data.size());
 	{
 		DirAccessRef dar = DirAccess::open(temp_file.get_base_dir());
 		dar->remove(temp_file);
@@ -1554,7 +1554,7 @@ Error ResourceFormatSaverTextInstance::save(const String &p_path, const RES &p_r
 	}
 #endif
 
-	Vector<ResourceSort> sorted_er;
+	std::vector<ResourceSort> sorted_er;
 
 	for (Map<RES, int>::Element *E = external_resources.front(); E; E = E->next()) {
 
@@ -1564,7 +1564,7 @@ Error ResourceFormatSaverTextInstance::save(const String &p_path, const RES &p_r
 		sorted_er.push_back(rs);
 	}
 
-	sorted_er.sort();
+	std::sort(sorted_er.begin(), sorted_er.end());
 
 	for (int i = 0; i < sorted_er.size(); i++) {
 		String p = sorted_er[i].resource->get_path();
@@ -1681,7 +1681,7 @@ Error ResourceFormatSaverTextInstance::save(const String &p_path, const RES &p_r
 			NodePath owner = state->get_node_owner_path(i);
 			Ref<PackedScene> instance = state->get_node_instance(i);
 			String instance_placeholder = state->get_node_instance_placeholder(i);
-			Vector<StringName> groups = state->get_node_groups(i);
+			std::vector<StringName> groups = state->get_node_groups(i);
 
 			String header = "[node";
 			header += " name=\"" + String(name).c_escape() + "\"";
@@ -1699,7 +1699,10 @@ Error ResourceFormatSaverTextInstance::save(const String &p_path, const RES &p_r
 			}
 
 			if (groups.size()) {
-				groups.sort_custom<StringName::AlphCompare>();
+				// need_update : StringName::AlphCompare{} => StringName::AlphCompare
+
+				std::sort(groups.begin(), groups.end(), StringName::AlphCompare{});
+
 				String sgroups = " groups=[\n";
 				for (int j = 0; j < groups.size(); j++) {
 					sgroups += "\"" + String(groups[j]).c_escape() + "\",\n";
@@ -1763,7 +1766,7 @@ Error ResourceFormatSaverTextInstance::save(const String &p_path, const RES &p_r
 			f->store_line("]");
 		}
 
-		Vector<NodePath> editable_instances = state->get_editable_instances();
+		std::vector<NodePath> editable_instances = state->get_editable_instances();
 		for (int i = 0; i < editable_instances.size(); i++) {
 			f->store_line("\n[editable path=\"" + editable_instances[i].operator String() + "\"]");
 		}
