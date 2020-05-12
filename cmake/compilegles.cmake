adaec1ae7d4e3e06c85a3d1228b4eced3264b327
Flavien PERINEAU <feyleth@gmail.com>
2020-05-12 14:09:10 +0200

    correct error for macro

--- cmake/compilegles.cmake
+++ cmake/compilegles.cmake
@@ -1,5 +1,5 @@
 macro(compilegles2 source gen)
-list(APPEND gen "${CMAKE_CURRENT_SOURCE_DIR}/${source}.glsl.gen.h")
+list(APPEND ${gen} "${CMAKE_CURRENT_SOURCE_DIR}/${source}.glsl.gen.h")
 add_custom_command(OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/${source}.glsl.gen.h"
      COMMAND ${Python_EXECUTABLE} "${CMAKE_SOURCE_DIR}/script/build_gles2_headers.py" "${CMAKE_CURRENT_SOURCE_DIR}/${source}.glsl"
      DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${source}.glsl"
@@ -8,7 +8,7 @@ add_custom_command(OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/${source}.glsl.gen.h"
 endmacro()
 
 macro(compilegles3 source gen)
-list(APPEND gen "${CMAKE_CURRENT_SOURCE_DIR}/${source}.glsl.gen.h")
+list(APPEND ${gen} "${CMAKE_CURRENT_SOURCE_DIR}/${source}.glsl.gen.h")
 add_custom_command(OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/${source}.glsl.gen.h"
      COMMAND ${Python_EXECUTABLE} "${CMAKE_SOURCE_DIR}/script/build_gles3_headers.py" "${CMAKE_CURRENT_SOURCE_DIR}/${source}.glsl"
      DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${source}.glsl"
